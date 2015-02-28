85d1a7a27f371cd9dac2098a9927950c43a7ab22
Pierre-Luc BLOT <pierreluc.blot@gmail.com>
2015-02-28 19:56:31 +0100

    KAWATreeMethod compile

--- src/KAWATree/src/KAWATreeMethod.h
+++ src/KAWATree/src/KAWATreeMethod.h
@@ -8,10 +8,22 @@
 #ifndef KAWATreeMETHOD_H_
 #define KAWATreeMETHOD_H_
 
+#include <iostream>
+#include "KAWATreeBodyMethod.h" 
+
 class KAWATreeMethod {
+private:
+	std::string name;
+	KAWATreeBodyMethod body;
 public:
 	KAWATreeMethod();
 	virtual ~KAWATreeMethod();
+
+	std::string getName();
+	void setName(std::string newName);
+	KAWATreeBodyMethod getBody();
+	void setBody(KAWATreeBodyMethod newBody);
+
 };
 
 #endif /* METHODE_H_ */
